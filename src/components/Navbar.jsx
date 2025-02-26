0b12667d98d0bbadd438348d7d2c2e6b3446768a
alialishoaibali1254 <alialishoaibali719296@gmail.com>
2025-02-25 23:30:12 -0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,23 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-      <div className=" container nav_bar"
-      data-aos="fade-down"
-      data-aos-duration="1000"
-      >
-        <div className="left nav_item ">
-          SHOAIB
+      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+        <div className="container">
+          <a className="navbar-brand text-light fs-3" href="#home">SHOAIB</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <div className="navbar-nav ms-auto">
+              <a className="nav-item nav-link text-light fs-5" href="#home">Home</a>
+              <a className="nav-item nav-link text-light fs-5" href="#education">Education</a>
+              <a className="nav-item nav-link text-light fs-5" href="#skills">Skills</a>
+              <a className="nav-item nav-link text-light fs-5"  href="#project">Project</a>
+              <a className="nav-item nav-link text-light fs-5 " href="#contact">Contact</a>
+            </div>
+          </div>
         </div>
-        <div className="right-item ">
-       <a href="#home" className="nav_items">Home</a>
-       <a href="#education" className="nav_items">Education</a>
-       <a href="#skills" className="nav_items">Skills</a>
-       <a href="#project" className="nav_items">Project</a>
-       <a href="#contact" className="nav_items">Contact</a>
-        </div>
-      </div>
+      </nav>
     </>
   )
 }
