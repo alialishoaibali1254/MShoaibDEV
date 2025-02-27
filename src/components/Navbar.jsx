d43e6bc31c9e08eb4d01d591928c90e915681cc9
alialishoaibali1254 <alialishoaibali719296@gmail.com>
2025-02-26 22:50:35 -0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
-        <div className="container">
+        <div className="container-fluid">
           <a className="navbar-brand text-light fs-3" href="#home">SHOAIB</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
